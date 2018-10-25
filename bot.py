954e89ae445ecddd75f53305859f1f9e9ed0ce49
SkillsRemoved <44251471+SkillsRemoved@users.noreply.github.com>
2018-10-24 20:45:54 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,14 @@
+import discord
+from discord.ext import commands
+import asyncio
 
+client= commands.Bot(command_prefix='c!')
+
+@client.event
+async def on_ready():
+    print('The bot is ready!')
+    print(client.user.name)
+    print(client.user.id)
+    print('----------------------------')
+
+client.run ('NTAyMzE4NzY3NzM3NTM2NTEy.DrLN5g.OsjA1dH_T-82Q-BLDkb2xvy34BY')
